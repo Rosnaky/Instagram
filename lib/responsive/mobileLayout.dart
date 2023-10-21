175b5dd27e046b4ab3752c1878baa6c1b735082f
Ronak Patel <ronakjpatelr22@gmail.com>
2023-10-21 17:53:54 -0400

    added screen ui

--- lib/responsive/mobileLayout.dart
+++ lib/responsive/mobileLayout.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/models/user.dart';
 import 'package:instagram_flutter/providers/UserProvider.dart';
+import 'package:instagram_flutter/utils/Colours.dart';
 import 'package:provider/provider.dart';
 
 class MobileLayout extends StatefulWidget {
@@ -11,10 +13,79 @@ class MobileLayout extends StatefulWidget {
 }
 
 class _MobileLayoutState extends State<MobileLayout> {
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int value) {
+    pageController.jumpToPage(value);
+  }
+
+  void onPageChanged(int value) {
+    setState(() {
+      _page = value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     User user = Provider.of<UserProvider>(context).getUser;
 
-    return Scaffold(body: Center(child: Text(user.name)));
+    return Scaffold(
+      body: PageView(
+        children: [
+          Text("home"),
+          Text("seach"),
+          Text("feed"),
+          Text("favourites"),
+          Text("profile"),
+        ],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
+      ),
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: mobileBackgroundColor,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home,
+                  color: _page == 0 ? primaryColor : secondaryColor),
+              label: "",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search,
+                  color: _page == 1 ? primaryColor : secondaryColor),
+              label: "",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.add_circle,
+                  color: _page == 2 ? primaryColor : secondaryColor),
+              label: "",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite,
+                  color: _page == 3 ? primaryColor : secondaryColor),
+              label: "",
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+                  color: _page == 4 ? primaryColor : secondaryColor),
+              label: "",
+              backgroundColor: primaryColor),
+        ],
+        onTap: navigationTapped,
+      ),
+    );
   }
 }
